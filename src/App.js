2bc92bd691415ddaf0251394b260e371f256c411
PragatiBagul <bagulpragati15@gmail.com>
2022-02-03 20:17:12 +0530

    Added editable feature to TaskList

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Task from './TaskApp/Task';
 import { List } from "@mui/material";
+import { useState } from 'react';
 function App() {
+  const [task, setTask] = useState();
   return (
     <div className="App">
       <List>
-        <Task task={"Hello"} index={ "1"}/>
-        <Task task={"Hello"} index={ "2"}/>
-        <Task task={"Hello"} index={ "3"}/>
-        <Task task={"Hello"} index={ "4"}/>
-        <Task task={"Hello"} index={ "5"}/>
+        <Task task={task} setTask={setTask} index={ "1"}/>
         </List> 
     </div>
   );
--- src/TaskApp/Task.js
+++ src/TaskApp/Task.js
@@ -1,9 +1,11 @@
-import { Checkbox, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+import { Checkbox, IconButton, ListItem, ListItemButton, ListItemIcon, ListItemText,TextField } from "@mui/material";
 import { useState } from 'react';
 import './Task.css';
+import DoneOutlinedIcon from '@mui/icons-material/DoneOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
-const Task = ({ task, index }) => {
+const Task = ({ task, setTask,index }) => {
     const [isCompleted, setIsCompleted] = useState(false);
+    const [isEditable, setIsEditable] = useState(false);
     const completeTask = (index) => {
         if (document.getElementById(`checkbox-${index}`).checked)
         {
@@ -14,12 +16,55 @@ const Task = ({ task, index }) => {
             setIsCompleted(false);
         }
     }
+    const handleChange = ({target}) => {
+        setTask(target.value);
+    }
     return (<ListItem disablePadding>        
         <ListItemButton>
-            <Checkbox edge="start" id={`checkbox-${index}`} onChange={() => completeTask(index)}/>
-            <ListItemText className={isCompleted ? 'completed':'incomplete'} id={index} primary={`${ task }`} />
+            <Checkbox edge="start" id={`checkbox-${index}`} onChange={() => completeTask(index)} />
+            {
+                isEditable ?
+                    <>
+                        <TextField
+                            fullWidth
+                            type="text"
+                            size="small"
+                            defaultValue={task}
+                            onChange={handleChange}
+                        />
+                        <IconButton edge="end"
+                            onClick={() => setIsEditable(false)}
+                            color="primary">
+                            <DoneOutlinedIcon />
+                        </IconButton>
+                    </> :
+                    <>
+                        <ListItemText
+                            className={isCompleted ? 'completed' : 'incomplete'}
+                            id={index}>
+                            <span>{task}</span>
+                        </ListItemText>
+                        <IconButton edge="end" onClick={() => setIsEditable(true)} color="primary">
+                    <EditOutlinedIcon/>
+                </IconButton></>
+            }
         </ListItemButton>
     </ListItem>);
 }
  
 export default Task;
+
+/*          
+             {
+                    isEditable ?
+                        (
+                        <IconButton edge="end" onClick={setIsEditable(false)} color="primary">
+                    <DoneOutlinedIcon/>
+                </IconButton>
+                    </>
+                        ) : ( <><ListItemText className={isCompleted ? 'completed' : 'incomplete'} id={index}>
+                        <span>{task}</span></ListItemText><IconButton edge="end" onClick={setIsEditable(true)} color="primary">
+                    <EditOutlinedIcon/>
+                </IconButton></>)
+                }   
+        </ListItemButton> */
